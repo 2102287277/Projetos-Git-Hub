4cd5f825f439e38a52056e2bc15a828a66fb764b
ROBERTO HIROSHI ICHIKAWA <2102287277@viavarejo.br>
2019-03-16 13:01:24 -0300

    Primeiro

---
+++ src/teste001/AloMundo.java
@@ -0,0 +1,26 @@
+package teste001;
+
+import java.util.Scanner;
+
+public class AloMundo {
+
+	public static void main(String[] args) {
+	int v1 = 12;
+		int v2 = 0;
+		System.out.print("Digite um nro");
+		Scanner tec = new Scanner(System.in),
+		int codigoDigitado = scanner.nextInt(),
+		
+		v2 = v1 % 2;
+		if (v2 == 0) {		
+			System.out.print("É par");
+		}else {
+			System.out.print("É impar"); 
+	
+		}
+	}
+}
+
+
+
+gerar equals hashcode
